3093d1bd16b692212ff885dce902cab9a77d6728
FedeLopez17 <fedelopez1712@gmail.com>
2024-03-10 17:48:15 -0300

    Feat - Improve product card layout

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -31,11 +31,16 @@ export default function Product({ product }: { product: ProductType }) {
     <Link
       to={`/product/${product.categoryId}/${product.subCategoryId}/${product.slug}`}
     >
-      <section className="flex flex-col bg-red-500 p-2 box-border w-[290px] aspect-[9/12]">
+      <section className="flex flex-col bg-red-500 p-2 box-border w-[290px] h-[380px] overflow-auto">
         {imageLoaded ? <img src={productImage} /> : <ImageLoadingSkeleton />}
         <p>{product.name}</p>
-        <p>${product.price}</p>
-        <RatingStars rating={product.rating} />
+        <section className="flex justify-between mt-auto">
+          <section className="flex items-center gap-2 text-xs">
+            <RatingStars rating={product.rating} />
+            <p>({product.rating})</p>
+          </section>
+          <p className="text-xl font-bold">${product.price}</p>
+        </section>
       </section>
     </Link>
   );
