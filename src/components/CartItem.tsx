8269a9083b4f1034708d6b2947c2b66735e8021d
FedeLopez17 <fedelopez1712@gmail.com>
2024-03-10 15:43:07 -0300

    Feat - Make each cart item a link to the item page, add a loading skeleton for the image, and display the quantity of the item

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,6 +2,8 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import { ShoppingCartItem, RemoveProduct } from "../types";
 import { useEffect, useState } from "react";
 import { formatPrice, getProductImage } from "../utils";
+import { Link } from "react-router-dom";
+import ImageLoadingSkeleton from "./ImageLoadingSkeleton";
 
 export default function CartItem({
   removeProduct,
@@ -11,29 +13,53 @@ export default function CartItem({
   cartItem: ShoppingCartItem;
 }) {
   const [productImage, setProductImage] = useState<string>();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
-    getProductImage(product.imagesPath).then((imageResponse) =>
-      setProductImage(imageResponse)
-    );
-  }, []);
+    const preloadImage = async () => {
+      try {
+        const imageUrl = await getProductImage(product.imagesPath);
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => {
+          setProductImage(imageUrl);
+          setImageLoaded(true);
+        };
+      } catch (error) {
+        console.error("Error loading image:", error);
+      }
+    };
+
+    preloadImage();
+  }, [product.imagesPath]);
 
   const totalPrice = product.price * quantity;
 
   return (
-    <section className="flex gap-4 bg-red-400 w-full lg:w-[500px] p-2 pr-4 items-center justify-center">
-      <section className="flex w-24 h-24">
-        <img src={productImage} alt="Product Image" />
+    <Link
+      to={`/product/${product.categoryId}/${product.subCategoryId}/${product.slug}`}
+    >
+      <section className="flex gap-4 bg-red-400 w-full lg:w-[500px] p-2 pr-4 items-center justify-center">
+        <section className="flex w-24 h-24">
+          {imageLoaded ? (
+            <img src={productImage} alt="Product Image" />
+          ) : (
+            <ImageLoadingSkeleton />
+          )}
+        </section>
+        <section className="bg-lime-300 flex-1">
+          <p>{product.name}</p>
+          <p>
+            <span className="font-bold">Quantity:</span> {quantity}
+          </p>
+          {quantity > 1 && <p>{`${formatPrice(product.price)} per unit`}</p>}
+          <p className="font-bold">{formatPrice(totalPrice)}</p>
+        </section>
+        <FaRegTrashAlt
+          className="cursor-pointer"
+          onClick={() => removeProduct(product)}
+        />
       </section>
-      <section className="bg-lime-300 flex-1">
-        <p>{product.name}</p>
-        {quantity > 1 && <p>{`${formatPrice(product.price)} per unit`}</p>}
-        <p className="font-bold">{formatPrice(totalPrice)}</p>
-      </section>
-      <FaRegTrashAlt
-        className="cursor-pointer"
-        onClick={() => removeProduct(product)}
-      />
-    </section>
+    </Link>
   );
 }
