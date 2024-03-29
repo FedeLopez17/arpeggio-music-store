8bae0cd32ee7e06b3edbc96645b912c6e03cf96c
FedeLopez17 <fedelopez1712@gmail.com>
2024-03-28 23:53:13 -0300

    Feat - Implement first barebones version of the home-page, still missing sections

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,11 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import MainNavBarLink from "../components/MainNavBarLink";
 import { FaBars, FaInfoCircle, FaShoppingCart } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
-import { GoHomeFill } from "react-icons/go";
 import { PiGuitarFill } from "react-icons/pi";
 import { ShoppingCart } from "../types";
 import SearchBar from "../components/SearchBar";
+import navBarLogo from "../assets/images/arpeggio.svg";
 
 export default function RootLayout({
   openSideBar,
@@ -29,47 +29,57 @@ export default function RootLayout({
 
   return (
     <section className="min-h-screen flex flex-col">
-      <nav className="p-3 bg-pink-400 flex flex-col items-center gap-4 relative">
-        <ul className="flex justify-center items-center gap-8">
-          {isCatalogPage && (
-            <li className="xl:hidden cursor-pointer absolute left-2">
-              {sideBarActive ? (
-                <FaXmark onClick={closeSideBar} />
-              ) : (
-                <FaBars onClick={openSideBar} />
-              )}
+      <nav className="p-3 bg-black text-white flex flex-col items-center gap-4 relative">
+        <section className="w-full xl:w-[1280px] flex box-border px-4 items-center">
+          <Link to="/">
+            <img
+              src={navBarLogo}
+              alt="Arpeggio Music Store's Logo"
+              title="Home"
+              className="h-6"
+            />
+          </Link>
+          <ul className="flex justify-end items-center gap-8 w-full">
+            {isCatalogPage && (
+              <li className="xl:hidden cursor-pointer absolute left-2">
+                {sideBarActive ? (
+                  <FaXmark onClick={closeSideBar} />
+                ) : (
+                  <FaBars onClick={openSideBar} />
+                )}
+              </li>
+            )}
+            <li>
+              <MainNavBarLink
+                path="/catalog/1"
+                innerText="Catalog"
+                pathToMatch="/catalog/"
+                icon={PiGuitarFill}
+              />
             </li>
-          )}
-          <li>
-            <MainNavBarLink path="/" innerText="Home" icon={GoHomeFill} />
-          </li>
-          <li>
-            <MainNavBarLink
-              path="/catalog/1"
-              innerText="Catalog"
-              pathToMatch="/catalog/"
-              icon={PiGuitarFill}
-            />
-          </li>
-          <li className="hidden md:block">
-            <SearchBar searchBarValue={searchBarValue} setSearch={setSearch} />
-          </li>
-          <li>
-            <MainNavBarLink
-              path="/about"
-              innerText="About"
-              icon={FaInfoCircle}
-            />
-          </li>
-          <li>
-            <MainNavBarLink
-              path="/cart"
-              innerText="Cart"
-              icon={FaShoppingCart}
-              addNotification={cartHasItems}
-            />
-          </li>
-        </ul>
+            <li className="hidden md:block">
+              <SearchBar
+                searchBarValue={searchBarValue}
+                setSearch={setSearch}
+              />
+            </li>
+            <li>
+              <MainNavBarLink
+                path="/cart"
+                innerText="Cart"
+                icon={FaShoppingCart}
+                addNotification={cartHasItems}
+              />
+            </li>
+            <li>
+              <MainNavBarLink
+                path="/about"
+                innerText="About"
+                icon={FaInfoCircle}
+              />
+            </li>
+          </ul>
+        </section>
         <section className="md:hidden">
           <SearchBar searchBarValue={searchBarValue} setSearch={setSearch} />
         </section>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,65 @@
+import HeroSlideshow from "../components/HeroSlideshow";
+import MainCategoryCard from "../components/MainCategoryCard";
+import keys from "../assets/images/categories/keys.png";
+import piano from "../assets/images/categories/piano.png";
+import electricGuitar from "../assets/images/categories/electric-guitar.png";
+import acousticGuitar from "../assets/images/categories/acoustic-guitar.png";
+import classicalGuitar from "../assets/images/categories/classical-guitar.png";
+import bass from "../assets/images/categories/bass.png";
+import guitarPedal from "../assets/images/categories/guitar-pedal.png";
+import sax from "../assets/images/categories/sax.png";
+import percussion from "../assets/images/categories/percussion.png";
+import amp from "../assets/images/categories/amp.png";
+import drums from "../assets/images/categories/drums.png";
+import { CategoryCard } from "../types";
+
+const CATEGORIES: CategoryCard[] = [
+  { image: drums, linkTo: "/catalog/drums/1", text: "DRUMS" },
+  { image: percussion, linkTo: "/catalog/percussion/1", text: "PERCUSSION" },
+  { image: sax, linkTo: "/catalog/saxes/1", text: "SAXES" },
+  {
+    image: electricGuitar,
+    linkTo: "/catalog/guitars_basses/electric/1",
+    text: "ELECTRIC GUITARS",
+  },
+  {
+    image: acousticGuitar,
+    linkTo: "/catalog/guitars_basses/acoustic/1",
+    text: "ACOUSTIC GUITARS",
+  },
+  {
+    image: classicalGuitar,
+    linkTo: "/catalog/guitars_basses/classical/1",
+    text: "CLASSICAL GUITARS",
+  },
+  {
+    image: bass,
+    linkTo: "/catalog/guitars_basses/electric_basses/1",
+    text: "BASSES",
+  },
+  { image: piano, linkTo: "/catalog/keys/pianos/1", text: "PIANOS" },
+  { image: keys, linkTo: "/catalog/keys/keyboards/1", text: "KEYBOARDS" },
+  { image: amp, linkTo: "/catalog/amplification/1", text: "AMPS" },
+  { image: guitarPedal, linkTo: "/catalog/accessories/1", text: "ACCESSORIES" },
+];
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <HeroSlideshow />
+      <section className="w-full xl:w-[1280px]">
+        <h1>Main Categories</h1>
+        <section className="w-full bg-red-500 grid grid-cols-home-categories gap-1 justify-center justify-items-center py-4">
+          {CATEGORIES.map((category) => (
+            <MainCategoryCard
+              key={category.image}
+              image={category.image}
+              linkTo={category.linkTo}
+              text={category.text}
+            />
+          ))}
+        </section>
+      </section>
+    </section>
+  );
 }
