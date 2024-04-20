0406cdd10fe9a0b5aff37e85a19e8895df7ad1f1
FedeLopez17 <fedelopez1712@gmail.com>
2024-04-20 17:44:49 -0300

    Feat - Improve product not found page

--- src/components/ProductNotFound.tsx
+++ src/components/ProductNotFound.tsx
@@ -1,3 +1,17 @@
+import produtNotFound from "../assets/images/not-found-404.png";
+
 export default function ProductNotFound() {
-  return <h1>Sorry, this product couldn't be found. (404)</h1>;
+  return (
+    <section className="flex flex-col items-center flex-grow">
+      <img
+        src={produtNotFound}
+        alt="404 Error"
+        className="w-[min(300px,60%)] aspect-square mt-20"
+      />
+      <h4 className="font-bold text-3xl mt-8 mb-2 px-2">
+        Sorry, this product
+        <span className="text-purple-500"> couldn't be found</span>!
+      </h4>
+    </section>
+  );
 }
