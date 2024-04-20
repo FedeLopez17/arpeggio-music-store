a0ef3902c2a8bb3a5e60cb51ecac58aa5b0a3ba6
FedeLopez17 <fedelopez1712@gmail.com>
2024-04-19 23:56:12 -0300

    Refactor - Address some minor styling issues and slightly improve overall styling

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -11,6 +11,7 @@ import {
   RemoveProduct,
   AddFavorite,
   RemoveFavorite,
+  ProductType,
 } from "../types";
 import { useEffect, useState } from "react";
 import CartButton from "../components/CartButton";
@@ -68,12 +69,56 @@ export default function ProductPage({
     classes?: string;
   }) => (
     <section className={`flex flex-col ${classes}`}>
-      <h1 className="text-xl font-bold">{name}</h1>
+      <h1 className="text-2xl font-bold">{name}</h1>
       <span className="flex gap-2 items-center text-sm">
         <p className="opacity-70">{rating}</p>
         <RatingStars rating={rating} classes="text-sm text-purple-700" />
       </span>
       <h3 className="text-xl mt-2">{formatPrice(price)}</h3>
+    </section>
+  );
+
+  const FavAndCartButtons = ({
+    isAdd,
+    productData,
+    classes,
+  }: {
+    isAdd: boolean;
+    productData: ProductType;
+    classes?: string;
+  }) => (
+    <section
+      className={`flex gap-1 justify-center items-center w-full md:w-[250px] md:self-start lg:mb-44 ${classes}`}
+    >
+      <FavoriteToggle
+        addToFavorites={() => addFavorite(productData.slug)}
+        removeFromFavorites={() => removeFavorite(productData.slug)}
+        isFavorite={isFavorite}
+        classes="mr-4"
+      />
+      {!isAdd ? (
+        <CartButton
+          innerText={"REMOVE FROM CART"}
+          callBack={() => removeProduct(productData)}
+          classes="flex-1"
+        />
+      ) : (
+        <>
+          <select
+            className="w-1/4 h-full bg-slate-200"
+            id="quantity-select"
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+          >
+            <NumericSelectOptions />
+          </select>
+          <CartButton
+            innerText={"ADD TO CART"}
+            callBack={() => addProduct({ product: productData, quantity })}
+            classes="flex-1"
+          />
+        </>
+      )}
     </section>
   );
 
@@ -85,19 +130,33 @@ export default function ProductPage({
     <section className="flex justify-center">
       <section className="flex flex-col lg:flex-row w-full xl:w-[1280px] justify-center gap-24 pb-32">
         <section className=" flex flex-col items-center px-6 mt-10">
-          <section className="w-full md:w-[600px] flex flex-col gap-24">
-            <NameAndPrice
-              name={productData.name}
-              price={productData.price}
-              rating={productData.rating}
-              classes="lg:hidden"
-            />
+          <section className="w-full md:w-[600px] flex flex-col gap-10 lg:gap-24">
+            <h1 className="text-2xl font-bold lg:hidden">{productData.name}</h1>
+
             <Slideshow imageUrls={imageURLs} />
+
+            <section className="lg:hidden">
+              <span className="flex gap-2 items-center text-sm">
+                <p className="opacity-70">{productData.rating}</p>
+                <RatingStars
+                  rating={productData.rating}
+                  classes="text-sm text-purple-700"
+                />
+              </span>
+              <h3 className="text-xl mt-2">{formatPrice(productData.price)}</h3>
+            </section>
+
+            <FavAndCartButtons
+              isAdd={!productInCart}
+              productData={productData}
+              classes={"lg:hidden"}
+            />
+
             <AttributesTable attributes={productData.attributes} />
           </section>
         </section>
         <section className="flex flex-col items-center">
-          <section className="w-full md:w-fit flex items-center flex-col gap-4 mt-10 sticky top-10">
+          <section className="w-full md:w-[300px] flex items-start flex-col gap-4 lg:mt-10 sticky top-10">
             <NameAndPrice
               name={productData.name}
               price={productData.price}
@@ -105,47 +164,11 @@ export default function ProductPage({
               classes="hidden lg:flex"
             />
 
-            <section className="flex gap-1 justify-center items-center w-11/12 md:w-[250px] md:self-start mb-44">
-              {productInCart ? (
-                <>
-                  <FavoriteToggle
-                    addToFavorites={() => addFavorite(productData.slug)}
-                    removeFromFavorites={() => removeFavorite(productData.slug)}
-                    isFavorite={isFavorite}
-                    classes="mr-4"
-                  />
-                  <CartButton
-                    innerText={"REMOVE FROM CART"}
-                    callBack={() => removeProduct(productData)}
-                    classes="flex-1"
-                  />
-                </>
-              ) : (
-                <>
-                  <FavoriteToggle
-                    addToFavorites={() => addFavorite(productData.slug)}
-                    removeFromFavorites={() => removeFavorite(productData.slug)}
-                    isFavorite={isFavorite}
-                    classes="mr-4"
-                  />
-                  <select
-                    className="w-1/4 h-full bg-slate-200"
-                    id="quantity-select"
-                    value={quantity}
-                    onChange={(e) => setQuantity(Number(e.target.value))}
-                  >
-                    <NumericSelectOptions />
-                  </select>
-                  <CartButton
-                    innerText={"ADD TO CART"}
-                    callBack={() =>
-                      addProduct({ product: productData, quantity })
-                    }
-                    classes="flex-1"
-                  />
-                </>
-              )}
-            </section>
+            <FavAndCartButtons
+              isAdd={!productInCart}
+              productData={productData}
+              classes={"hidden lg:flex"}
+            />
           </section>
         </section>
       </section>
