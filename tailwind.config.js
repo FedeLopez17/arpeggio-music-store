9d9d0a89db9a675cc1974125fac7be7724b4b70d
FedeLopez17 <fedelopez1712@gmail.com>
2024-04-30 20:41:05 -0300

    Fix - Fix product banner background image paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ export default {
         "radial-gradient-hero-slideshow":
           "radial-gradient(ellipse at center, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 90%,rgba(0,0,0,1) 100%)",
         "banner-shape-left":
-          "url('./src/assets/images/banners/bg-shape-left.svg'), linear-gradient(45deg, rgb(162 102 222) , transparent)",
+          "url('/src/assets/images/banners/bg-shape-left.svg'), linear-gradient(45deg, rgb(162 102 222) , transparent)",
         "banner-shape-right":
-          "url('./src/assets/images/banners/bg-shape-right.svg'), linear-gradient(45deg, transparent, rgb(141 145 149))",
+          "url('/src/assets/images/banners/bg-shape-right.svg'), linear-gradient(45deg, transparent, rgb(141 145 149))",
       },
       keyframes: {
         "fade-in": {
