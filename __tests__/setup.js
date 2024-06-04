ad86f71f1e92ca247ff7112dab851c58c7864e0b
FedeLopez17 <fedelopez1712@gmail.com>
2024-06-04 17:00:31 -0300

    Fix - Remove unnecessary import

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -2,7 +2,6 @@ import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/extend-expect";
 
 expect.extend(matchers);
 
