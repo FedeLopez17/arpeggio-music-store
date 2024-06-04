befc519c4148de35b5c3beb74640434951765e50
FedeLopez17 <fedelopez1712@gmail.com>
2024-06-04 16:12:11 -0300

    Fix - minor config fix

--- __tests__/Footer.test.tsx
+++ __tests__/Footer.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../src/components/Footer";
-import { beforeAll, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
