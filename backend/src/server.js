3d2431147ab91ffefad73ec7653ac09b14255c7c
FedeLopez17 <fedelopez1712@gmail.com>
2026-01-21 06:37:15 -0300

    Feat: Connect MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
