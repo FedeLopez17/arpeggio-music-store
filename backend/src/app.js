85f11cb5a5166610fd8d7bcbb4642996a136dcbe
FedeLopez17 <fedelopez1712@gmail.com>
2026-01-21 06:08:21 -0300

    Feat: Add initial backend structure

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+module.exports = app;
